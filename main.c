f6e6c940d1408fd94f6104687dba8b639fc4a6cb
sunhang <sunhang09@gmail.com>
2020-07-29 10:30:10 +0800

    first commit

---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "math.h"
+
+int main() {
+	int result = add(1, 2);
+	printf("result: %d\n", result);
+	return 0;
+}
---
+++ transform.c
@@ -0,0 +1,5 @@
+#include "transform.h"
+
+int change(int a) {
+	return a * 2;
+}
